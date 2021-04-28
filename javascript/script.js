a29ee1cb51420ce30c816f5452803da061e707f4
gregjett92 <greg.jett25@gmail.com>
2021-04-27 23:44:01 -0500

    added images to take charge

--- javascript/script.js
+++ javascript/script.js
@@ -28,3 +28,15 @@ function openPopup(cardNum) {
     BG.style.display = 'none';
   }
   
+
+  // NAVBAR STICK
+  var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
